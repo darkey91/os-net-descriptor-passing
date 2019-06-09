b5e4bf9a9866e317a9486da295b9750c3419c0de
Di <eglet91@gmail.com>
2019-06-09 22:37:55 +0300

    first commit

---
+++ client.cpp
@@ -0,0 +1,91 @@
+#define SOCKET_NAME "socket"
+
+#include <iostream>
+
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+static const int BUF_SIZE = 4096;
+static std::string msg = "Hello, world!";
+
+void ext(const char *msg, int socket) {
+    std::cerr << msg << strerror(errno) << "\n";
+    close(socket);
+    exit(EXIT_FAILURE);
+}
+
+bool send(int sock_fd, const std::string &msg) {
+    ssize_t size = static_cast<ssize_t>(msg.size());
+    ssize_t was_sent = 0, left = size;
+
+    int tries = 5;
+
+    while (left > 0 && tries > 0) {
+        ssize_t sent = write(sock_fd, msg.data() + was_sent, left);
+        if (sent == -1) {
+            return false;
+        }
+        if (sent == 0) {
+            --tries;
+        }
+
+        was_sent += sent;
+        left -= sent;
+    }
+    return was_sent == size;
+}
+
+bool get_response(int sock_fd, std::string &response) {
+    char buffer[BUF_SIZE];
+    bzero(buffer, BUF_SIZE);
+
+    ssize_t r = read(sock_fd, buffer, BUF_SIZE);
+    if (r == -1) {
+         return false;
+    }
+    for (ssize_t i = 0; i < r; ++i) {
+        response += (char) buffer[i];
+     }
+
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        msg = std::move(argv[1]);
+    }
+
+    struct sockaddr_un sock_domain;
+    bzero(&sock_domain, sizeof(sock_domain));
+    sock_domain.sun_family = AF_UNIX;
+    strncpy(sock_domain.sun_path, SOCKET_NAME, sizeof (sock_domain.sun_path));
+
+    int sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock_fd == -1) {
+        ext("socket() failed: ", sock_fd);
+    }
+
+    if (connect(sock_fd, reinterpret_cast<sockaddr *>(&sock_domain), sizeof(sock_domain)) == -1) {
+        ext("connect() failed: ", sock_fd);
+    }
+    if (!send(sock_fd, msg)) {
+        ext("send() failed:", sock_fd);
+    }
+
+    std::string response = "Response: \n";
+    if (!get_response(sock_fd, response)) {
+        ext("get_response() failed: ", sock_fd);
+    }
+    std::cout << response;
+
+    close(sock_fd);
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,103 @@
+#define SOCKET_NAME "socket"
+#define BACKLOG 5
+
+#include <iostream>
+
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/un.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+static const int BUF_SIZE = 4096;
+
+void ext(const char *msg, int socket) {
+    std::cerr << msg << strerror(errno) << "\n";
+    close(socket);
+    exit(EXIT_FAILURE);
+}
+
+int accept(int sock_fd) {
+    int fd = accept(sock_fd, nullptr, nullptr);
+    if (fd == -1) {
+        ext("accept() failed: ", sock_fd);
+    }
+    return fd;
+}
+
+bool send(int fd, char buffer[], ssize_t len) {
+    std::cout << "sending...\n";
+    ssize_t left = len, was_sent = 0;
+
+    int tries = 5;
+
+    while (left > 0 && tries > 0) {
+        ssize_t sent = write(fd, buffer + was_sent, left);
+        if (sent == -1) {
+            return false;
+        }
+        if (sent == 0) {
+            --tries;
+        }
+
+        was_sent += sent;
+        left -= sent;
+    }
+    return was_sent == len;
+}
+
+ssize_t process_request(int fd) {
+   char buffer[BUF_SIZE] ;
+   memset(buffer, '\0', BUF_SIZE);
+   ssize_t size = 0;
+
+   while (true) {
+       ssize_t got = read(fd, buffer, BUF_SIZE);
+       if (got == 0) break;
+       if (got == -1 || !send(fd, buffer, got)) {
+            return -1;
+       }
+       size += got;
+   }
+   return size;
+}
+
+int main(int argc, char* argv[]) {
+
+    unlink(SOCKET_NAME);
+
+    struct sockaddr_un sock_domain;
+    memset(&sock_domain, 0, sizeof(sock_domain));
+    sock_domain.sun_family = AF_UNIX;
+    strncpy(sock_domain.sun_path, SOCKET_NAME, sizeof(sock_domain.sun_path) - 1);
+
+    int socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    if (socket_fd == -1) {
+        ext("socket() failed: ", socket_fd);
+    }
+
+    if (bind(socket_fd, reinterpret_cast<sockaddr *>(&sock_domain), sizeof(sockaddr_un)) == -1) {
+        ext("bind() failed: ", socket_fd);
+    }
+
+    if (listen(socket_fd, BACKLOG) == -1) {
+        ext("listen() failed: ", socket_fd);
+    }
+
+    std::cout << "Ready to serve!\n";
+    for (int req = 0; req < BACKLOG; ++req) {
+        int client_fd = accept(socket_fd);
+        process_request(client_fd);
+        close(client_fd);
+    }
+
+    unlink(SOCKET_NAME);
+    return 0;
+}
